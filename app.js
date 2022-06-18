47c941373326f37a54c7a403f340170cd15bf044
ayushchourasia01 <ayushchourasia01@gmail.com>
2022-06-19 05:07:06 +0530

    Finalcommit

--- app.js
+++ app.js
@@ -86,6 +86,10 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(process.env.PORT || 3000, function() {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
